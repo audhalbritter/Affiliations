b58c4a0d90d303bd8e7431e3c90f5565bd24b79f
audhalbritter <audhalbritter.com>
2018-09-15 09:56:23 +0200

    initial commit

---
+++ Affiliation.R
@@ -0,0 +1,77 @@
+## ----runAffiliations
+
+# Load libraries
+library("tidyverse")
+library("googlesheets")
+
+gs_ls() 
+# which google sheets do you have access to?
+CM <- gs_title(x = "ClimMani protocols overview", verbose = TRUE)
+#download data
+affiliation <- gs_read(ss = CM, ws = "ClimMani people", range = cell_rows(1:114)) %>% as.tibble()
+
+# Prepare data
+affiliation <- affiliation %>% 
+  select(-full, -Remark, -FormattingTeam, -Affiliation1_full, -Affiliation2_full, -Affiliation3_full) %>% 
+  rename(First = FirstName, Last = LastName, Main = MainAuthors, ChapterL = ChapterLeader) %>%
+  # gather all affiliations in one column
+  gather(key = Number, value = Affiliation, Affiliation1,  Affiliation2, Affiliation3) %>% 
+  filter(!is.na(Affiliation)) %>% 
+  mutate(Country = ifelse(First == "Sean" & Affiliation == "Department of Botany and Biodiversity Research Centre, University of British Columbia, Vancouver, British Columbia V6T 1Z4, Canada", "Canada", Country)) %>% 
+  mutate(Country = ifelse(First == "Sean" & Affiliation == "Earth and Environmental Sciences Division, Los Alamos National Laboratory, Los Alamos, New Mexico 87545, USA", "USA", Country)) %>% 
+  mutate(Country = ifelse(First == "Sean" & Affiliation == "Biosphere 2 and Department of Ecology & Evolutionary Biology, University of Arizona, Tucson, Arizona 85721, USA", "USA", Country)) %>% 
+  mutate(Country = ifelse(First == "Benjamin" & Affiliation == "Environmental Change Institute, School of Geography and the Environment, University of Oxford, Oxford, UK", "UK", Country)) 
+  
+
+# Function to get affiliations ranked from 1 to n
+rankNR <- function(x){
+  su=sort(unique(x))
+  for (i in 1:length(su)) x[x==su[i]] = i
+  return(x)
+}
+
+
+
+NameAffList <- affiliation %>% 
+  filter(is.na(Main)) %>% # remove main authors
+  arrange(Last, Affiliation) %>% 
+  rowwise() %>% 
+  # extract the first letter of each first name and put a dot after each letter
+  #mutate(
+    #Initials = paste(stringi::stri_extract_all(regex = "\\b([[:alpha:]])", str = First, simplify = TRUE), collapse = ". "),
+    #Initials = paste0(Initials, ".")) %>%
+  ungroup() %>% 
+  # add a column from 1 to n
+  mutate(ID = 1:n()) %>%
+  group_by(Affiliation) %>% 
+  # replace ID with min number (same affiliations become the same number)
+  mutate(ID = min(ID)) %>% 
+  ungroup() %>% 
+  # use function above to assign new ID from 1 to n
+  mutate(ID = rankNR(ID)) %>%
+  #Paste Last and Initials
+  #mutate(name = paste0(Last, ", ", Initials)) 
+  # Past First and lat name
+  mutate(name = paste(First, Last, sep = " "))
+
+# Create a list with all Affiliations
+NameAffList %>% 
+  distinct(ID, Affiliation) %>% 
+  arrange(ID) %>% 
+  mutate(ID = paste0("^", ID, "^")) %>% 
+  mutate(Affiliation2 = paste(ID, Affiliation, sep = "")) %>% 
+  pull(Affiliation2) %>% 
+  paste(collapse = ", ")
+
+ 
+# Create a list with all names
+NameAffList %>%   
+  group_by(Last, name) %>% 
+  summarise(affs = paste(ID, collapse = ",")) %>% 
+  mutate(
+    affs = paste0("^", affs, "^"),
+    nameID = paste0(name, affs)     
+         ) %>% 
+  pull(nameID) %>% 
+  paste(collapse = ", ")
+
